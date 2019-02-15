57f05d1b6cd52e2fa9dd2e67c8dab62142b4cd27
yangshangwei <yangshangwei@inspur.com>
2019-02-16 02:17:06 +0800

    并发初窥,简单的并发案例引出并发问题

---
+++ src/main/java/com/artisan/ConcurrencyMasterApplication.java
@@ -0,0 +1,14 @@
+package com.artisan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrencyMasterApplication.class, args);
+	}
+
+}
+
